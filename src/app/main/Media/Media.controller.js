525de0a3d924cd0253ef9e10bcc8aff61d3795a9
janak <janak@bugzstudio.com>
2017-08-18 12:39:18 +0000

    git-svn-id: https://svn.bugzstudio.com:7777/svn/GPScannerAdmin@72 3ff773e8-363c-db4a-86ae-61a296765148

--- src/app/main/Media/Media.controller.js
+++ src/app/main/Media/Media.controller.js
@@ -207,7 +207,8 @@
                 // .withOption('deferRender', true)
                 .withOption('createdRow', createdRow)
                 // .withOption('bFilter', false)
-                .withOption('dom', 'rt<"bottom"<"left"<"length"l>><"right"<"info"i><"pagination"p>>>')
+                // .withOption('dom', 'rt<"bottom"<"left"<"length"l>><"right"<"info"i><"pagination"p>>>')
+                .withOption('dom', 'rt<"bottom"<"left"<"length"l><"info"i>><"right"<"pagination"p>>>')
                 .withOption('scrollY', 'auto');
         });
 
