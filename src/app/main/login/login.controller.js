13e4c65e4586b604bdea90ee1e57eaf108b31fcc
jaimin <jaimin@bugzstudio.com>
2017-10-03 09:44:55 +0000

    set user name pass and remember me var
    git-svn-id: https://svn.bugzstudio.com:7777/svn/GPScannerAdmin@235 3ff773e8-363c-db4a-86ae-61a296765148

--- src/app/main/login/login.controller.js
+++ src/app/main/login/login.controller.js
@@ -17,62 +17,91 @@
                 RemeberMe: false
             }
 
+            var Remeber = $cookieStore.get('RemeberMe');
+            var Logout = $cookieStore.get('FlagLogout');
+            if (Remeber == true) {
+                $scope.model.UserName = $cookieStore.get('RemeberUserName');
+                $scope.model.Password = $cookieStore.get('RemeberPassword');
+                $scope.model.RemeberMe = true;
+            } else {
+                $scope.model.RemeberMe = false;
+                $cookieStore.remove('RemeberMe');
+                $cookieStore.remove('RemeberUserName');
+                $cookieStore.remove('RemeberPassword');
+                $cookieStore.remove('FlagLogout');
+
+            }
+
+            // if ($rootScope.flgLogout != true) {
+            //     $rootScope.flgLogout = false;
+            //     if (Remeber == 'true' && Logout == 'false') {
+            //         $scope.Login($scope.model, true, false);
+            //     }
+            // }
+
         }
 
-        $scope.Login = function(o) {
-            var params = {
-                username: o.UserName,
-                password: o.Password,
-                appId: localStorage.getItem('appId'),
-            }
-            $http.get($rootScope.RoutePath + "account/loginNew", { params: params }).then(function(data) {
-                if (data.data.success == true) {
+        $scope.Login = function(o, form, isLogout) {
+            if (form) {
+                $cookieStore.put('isAllowMultipleTabs', true);
+                sessionStorage.setItem('isLogin', true);
+                $cookieStore.put('RemeberMe', o.RemeberMe);
+                $cookieStore.put('RemeberUserName', o.UserName);
+                $cookieStore.put('RemeberPassword', o.Password);
+                $cookieStore.put('FlagLogout', false);
+                var params = {
+                    username: o.UserName,
+                    password: o.Password,
+                    appId: localStorage.getItem('appId'),
+                }
+                $http.get($rootScope.RoutePath + "account/loginNew", { params: params }).then(function(data) {
+                    if (data.data.success == true) {
 
-                    $scope.RoleWiseCountry = data.data.RolewiseCountryList;
-                    var CountryList = "";
-                    for (var i = 0; i < $scope.RoleWiseCountry.length; i++) {
-                        if (CountryList == "") {
-                            if ($scope.RoleWiseCountry[i] != "" && $scope.RoleWiseCountry[i] != null) {
-                                CountryList = $scope.RoleWiseCountry[i];
-                            }
-                        } else {
-                            if ($scope.RoleWiseCountry[i] != "" && $scope.RoleWiseCountry[i] != null) {
-                                CountryList = CountryList + "," + $scope.RoleWiseCountry[i];
+                        $scope.RoleWiseCountry = data.data.RolewiseCountryList;
+                        var CountryList = "";
+                        for (var i = 0; i < $scope.RoleWiseCountry.length; i++) {
+                            if (CountryList == "") {
+                                if ($scope.RoleWiseCountry[i] != "" && $scope.RoleWiseCountry[i] != null) {
+                                    CountryList = $scope.RoleWiseCountry[i];
+                                }
+                            } else {
+                                if ($scope.RoleWiseCountry[i] != "" && $scope.RoleWiseCountry[i] != null) {
+                                    CountryList = CountryList + "," + $scope.RoleWiseCountry[i];
+                                }
                             }
                         }
-                    }
-                    $scope.splitCountryList = CountryList.split(",")
-                    $cookieStore.put('UserImage', data.data.UserImage);
-                    $cookieStore.put('UserId', data.data.UserId);
-                    $cookieStore.put('UserCountry', data.data.UserCountry);
-                    $cookieStore.put('UserRoles', data.data.UserRoles);
-                    $rootScope.UserImage = $cookieStore.get('UserImage');
-                    $cookieStore.put('UserName', o.UserName);
-                    $cookieStore.put('token', data.data.token);
-                    $cookieStore.put('CountryList', _.uniq($scope.splitCountryList));
-                    // $cookieStore.put('appId', data.data.appId);
-                    $http.defaults.headers.common['Authorization'] = data.data.token; // jshint ignore:line
-                    $rootScope.MenuSet();
-                    $window.location.href = '/#/Dashboard';
+                        $scope.splitCountryList = CountryList.split(",")
+                        $cookieStore.put('UserImage', data.data.UserImage);
+                        $cookieStore.put('UserId', data.data.UserId);
+                        $cookieStore.put('UserCountry', data.data.UserCountry);
+                        $cookieStore.put('UserRoles', data.data.UserRoles);
+                        $rootScope.UserImage = $cookieStore.get('UserImage');
+                        $cookieStore.put('UserName', o.UserName);
+                        $cookieStore.put('token', data.data.token);
+                        $cookieStore.put('CountryList', _.uniq($scope.splitCountryList));
+                        // $cookieStore.put('appId', data.data.appId);
+                        $http.defaults.headers.common['Authorization'] = data.data.token; // jshint ignore:line
+                        $rootScope.MenuSet();
+                        $window.location.href = '/#/Dashboard';
 
-                    // $timeout(function() {
-                    //     $window.location.reload();
-                    // });
+                        // $timeout(function() {
+                        //     $window.location.reload();
+                        // });
 
 
-                    // window.location.href = "/#/Dashboard";
-                    // $state.go('app.Dashboard');
+                        // window.location.href = "/#/Dashboard";
+                        // $state.go('app.Dashboard');
 
-                } else {
-                    $mdToast.show(
-                        $mdToast.simple()
-                        .textContent(data.data.message)
-                        .position('top right')
-                        .hideDelay(3000)
-                    );
-                };
-            });
-
+                    } else {
+                        $mdToast.show(
+                            $mdToast.simple()
+                            .textContent(data.data.message)
+                            .position('top right')
+                            .hideDelay(3000)
+                        );
+                    };
+                });
+            }
         }
 
         $scope.ShowForgotPassword = function(ev, o) {
@@ -82,7 +111,7 @@
         $scope.ShowRegister = function(ev, o) {
             $state.go('app.Register');
         }
-
+        $scope.init();
 
         //////////
     }
