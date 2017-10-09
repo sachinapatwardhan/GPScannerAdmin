dff24ca8c734e421fc64a366c56d207e29f57b7d
jaimin <jaimin@bugzstudio.com>
2017-10-09 11:16:44 +0000

    Add days in expiry date column
    git-svn-id: https://svn.bugzstudio.com:7777/svn/GPScannerAdmin@256 3ff773e8-363c-db4a-86ae-61a296765148

--- src/app/main/Dashboard/Dashboard.controller.js
+++ src/app/main/Dashboard/Dashboard.controller.js
@@ -2048,7 +2048,7 @@
 
             var displaydata = "";
             if (days > 0) {
-                displaydata = days;
+                displaydata = days + ' days';
             }
             return displaydata;
 
