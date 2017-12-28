1a84a573ba8e993322e1497adb77789f4805715b
jay <jay@bugzstudio.com>
2017-12-28 11:38:13 +0000

    git-svn-id: https://svn.bugzstudio.com:7777/svn/GPScannerAdmin@331 3ff773e8-363c-db4a-86ae-61a296765148

--- src/app/main/WalletTransaction/WalletTransaction.controller.js
+++ src/app/main/WalletTransaction/WalletTransaction.controller.js
@@ -21,16 +21,18 @@
                 OrderNumber: '',
                 IsPaymentSuccess: 0,
                 PaymentType: 'Offline',
+                Country: $cookieStore.get('UserCountry')
             };
 
             $scope.modelSearch = {
                 Search: ''
             }
 
-            $scope.GetAllAppInfo();
+
             $scope.flag = false;
             $scope.IsShow = false;
             $scope.FlgImage = false;
+            $scope.GetAllAppInfo();
 
         }
 
@@ -83,24 +85,25 @@
                         formData.append(id, obj.lfFile);
                     });
                     $http.post($rootScope.RoutePath + "WalletTransaction/uploadImage", formData, {
-                            transformRequest: angular.identity,
-                            headers: {
-                                'Content-Type': undefined
-                            }
-                        }).then(function(data) {
-                            if (data.data.success == true) {
-                                $mdToast.show(
-                                    $mdToast.simple()
-                                    .textContent(data.data.message)
-                                    .position('top right')
-                                    .hideDelay(3000)
-                                );
-                                $scope.init();
-                                $scope.restForm();
-                            }
-                        })
-                        //$scope.GetAllUserWallet(true);
-                        //$scope.GetAllDynamicLocation(true);
+                        transformRequest: angular.identity,
+                        headers: {
+                            'Content-Type': undefined
+                        }
+                    }).then(function(data) {
+                        if (data.data.success == true) {
+                            $mdToast.show(
+                                $mdToast.simple()
+                                .textContent(data.data.message)
+                                .position('top right')
+                                .hideDelay(3000)
+                            );
+                            $scope.init();
+                            $scope.restForm();
+                            $scope.apiMedia.removeAll();
+                            $scope.WalletTransactionReload(true);
+
+                        }
+                    })
                 } else {
                     if (data.data.data == 'TOKEN') {
                         //$cookieStore.remove('UserName');
@@ -121,29 +124,10 @@
 
         };
 
-
-        //Filter
-        // $scope.toggle = function() {
-        //     // $scope.Reset()
-        //     if (!$scope.flgforIcon) {
-        //         $scope.flgforIcon = true;
-        //     } else {
-        //         $scope.flgforIcon = false;
-        //     }
-
-        //     $(function() {
-        //         $(".showBtn").toggleClass("active");
-        //         $(".ShowContentBox").slideToggle();
-        //     });
-        // };
-
         //Dynamic Pagging
 
 
-
-        $scope.FilterStatus = '';
-
-        $scope.dtUserColumns = [
+        $scope.dtWalletColumns = [
             DTColumnBuilder.newColumn(null).renderWith(NumberHtml).notSortable(),
             DTColumnBuilder.newColumn('OrderNumber'),
             DTColumnBuilder.newColumn('AppName'),
@@ -159,9 +143,8 @@
         ]
 
         $rootScope.CheckPageRights(($rootScope.state.current.ModuleName), function(response) {
-            $scope.dtUsercustomOptions = DTOptionsBuilder.newOptions().withOption('ajax', {
+            $scope.dtWalletcustomOptions = DTOptionsBuilder.newOptions().withOption('ajax', {
                     url: $rootScope.RoutePath + "WalletTransaction/GetAllWallettransaction",
-                    type: "get",
                     data: function(d) {
                         d.search = $scope.modelSearch.Search;
                         // if ($scope.modelSearch.idCountry == 'All') {
@@ -192,9 +175,9 @@
                         }
                         return d;
                     },
+                    type: "get",
                     dataSrc: function(json) {
                         if (json.success != false) {
-                            console.log(json.data)
                             $scope.lstWalletList = json.data;
                             $scope.recordsTotal = json.recordsTotal;
                             return json.data;
@@ -206,36 +189,42 @@
                 })
                 .withOption('processing', true) //for show progress bar
                 .withOption('serverSide', true) // for server side processing
-                .withPaginationType('simple') // for get full pagination options // first / last / prev / next and page numbers
+                .withPaginationType('full_numbers') // for get full pagination options // first / last / prev / next and page numbers
                 .withDisplayLength(25) // Page size
-                .withOption('aaSorting', [7, 'asc'])
+                .withOption('aaSorting', [8, 'asc'])
                 .withOption('responsive', true)
-                .withOption('autoWidth', true)
-                // .withOption('deferRender', true)
                 .withOption('createdRow', createdRow)
-                // .withOption('bFilter', false)
-                .withOption('dom', 'rt<"bottom"<"left"<"length"l>><"right"<"info"i><"pagination"p>>>')
+                // .withOption('dom', 'rt<"bottom"<"left"<"length"l>><"right"<"info"i><"pagination"p>>>')
+                .withOption('dom', 'rt<"bottom"<"left"<"length"l><"info"i>><"right"<"pagination"p>>>')
                 .withOption('scrollY', 'auto');
         });
-        $scope.dtUserInstance = {};
+        $scope.dtInstance = {}
 
-        // function actionsHtml(data, type, full, meta) {
-        //     var btns = '<div layout="row">';
-        //     if ($rootScope.FlgModifiedAccess) {
-        //         btns += '<md-button class="edit-button md-icon-button"  ng-click="EditWalletById(' + data.id + ')" aria-label="Edit Location">' +
-        //             '<md-icon md-font-icon="icon-pencil"  class="s18 green-500-fg"></md-icon>' +
-        //             '<md-tooltip md-visible="" md-direction="">Edit Wallet</md-tooltip>' +
-        //             '</md-button>';
-        //     }
-        //     if ($rootScope.FlgDeletedAccess) {
-        //         btns += '<md-button class="edit-button md-icon-button" ng-click="DeleteWalletById(' + data.id + ',false)" aria-label="Make Location InActive">' +
-        //             '<i class="icon-cancel s18 red-500-fg"></i>' +
-        //             '<md-tooltip md-visible="" md-direction="">Make Location InActive</md-tooltip>' +
-        //             '</md-button>';
-        //     }
-        //     btns += '</div>';
-        //     return btns;
-        // }
+        //Reload Datatable
+        $scope.WalletTransactionReload = function(IsUpdate) {
+
+            var resetPaging = false;
+            if (IsUpdate == true) {
+                resetPaging = true;
+            };
+
+            $scope.dtInstance.reloadData(callback, resetPaging);
+            $('#WalletTransactiontable').dataTable()._fnPageChange(0);
+            $('#WalletTransactiontable').dataTable()._fnAjaxUpdate();
+        }
+
+        $scope.reloadData = function() {}
+
+        function callback(json) {}
+
+        function createdRow(row, data, dataIndex) {
+            $compile(angular.element(row).contents())($scope);
+        }
+
+        function NumberHtml(data, type, full, meta) {
+            return (meta.row + 1);
+        }
+
         function StatusHtml(data, type, full, meta) {
             var status = '';
             if (data == 0) {
@@ -247,6 +236,30 @@
             }
             return status;
 
+        }
+
+        function DateHtml(data, type, full, meta) {
+            if (full != null && full != undefined && full != '') {
+                return $rootScope.convertdateformat(full.CreatedDate, 1);
+            } else {
+                return 'N/A';
+            }
+        }
+
+        function RemarkHtml(data, type, full, meta) {
+            if (data != null && data != undefined && data != '') {
+                return data;
+            } else {
+                return 'N/A';
+            }
+        }
+
+        function NullHtml(data, type, full, meta) {
+            if (data == null) {
+                return "N/A"
+            } else {
+                return data;
+            }
         }
 
         function actionsHtml(data, type, full, meta) {
@@ -289,88 +302,12 @@
                                 '</md-button>';
                         }
                     }
-
-                    // if ($rootScope.FlgModifiedAccess && full.tblorderservicestatus.OrderStatus == "Pending") {
-                    //     btns += '<md-button class="edit-button md-icon-button"  ng-click="ChangeStatus(' + data.id + ')" aria-label="">' +
-                    //         '<md-icon md-font-icon="icon-checkbox-marked-circle"  class="green-500-fg"></md-icon>' +
-                    //         '<md-tooltip md-visible="" md-direction="">Approve Order Service</md-tooltip>' +
-                    //         '</md-button>';
-                    // }
-
-                    // if ($rootScope.FlgModifiedAccess) {
-                    //     if (full.tblorderservicestatus != null && full.tblorderservicestatus != undefined && full.tblorderservicestatus != '') {
-                    //         var statusname = full.tblorderservicestatus.OrderStatus;
-                    //         if (statusname == "Approved") {
-                    //             btns += '<md-button class="edit-button md-icon-button"  ng-click="RenewOrderService(' + data.id + ')" aria-label="">' +
-                    //                 '<md-icon md-font-icon="icon-account-network"  class="blue-500-fg"></md-icon>' +
-                    //                 '<md-tooltip md-visible="" md-direction="">Renew Order Service</md-tooltip>' +
-                    //                 '</md-button>';
-
-                    //         }
-
-                    //         if (statusname != "Completed") {
-
-                    //             btns += '<md-button class="edit-button md-icon-button"  ng-click="EditDates(' + data.id + ')" aria-label="">' +
-                    //                 '<md-icon md-font-icon="icon-calendar-check-multiple" ></md-icon>' +
-                    //                 '<md-tooltip md-visible="" md-direction="">Update Date</md-tooltip>' +
-                    //                 '</md-button>';
-                    //         }
-                    //     }
-                    // }
                 }
                 btns += '</div>'
                 return btns;
             }
 
         };
-
-        function DateHtml(data, type, full, meta) {
-            if (full != null && full != undefined && full != '') {
-                return $rootScope.convertdateformat(full.CreatedDate, 1);
-            } else {
-                return 'N/A';
-            }
-        }
-
-        function RemarkHtml(data, type, full, meta) {
-            if (data != null && data != undefined && data != '') {
-                return data;
-            } else {
-                return 'N/A';
-            }
-        }
-
-        function NullHtml(data, type, full, meta) {
-            if (data == null) {
-                return "N/A"
-            } else {
-                return data;
-            }
-        }
-
-        $scope.reloadData = function() {}
-
-        function callback(json) {}
-
-        function createdRow(row, data, dataIndex) {
-            $compile(angular.element(row).contents())($scope);
-        }
-
-        function NumberHtml(data, type, full, meta) {
-            return (meta.row + 1);
-        }
-
-        //Reload Datatable
-        $scope.WalletTransaction = function(IsUpdate) {
-
-            var resetPaging = false;
-            if (IsUpdate == true) {
-                resetPaging = true;
-            };
-
-            $scope.dtUserInstance.reloadData(callback, resetPaging);
-            $('#WalletTransaction').dataTable()._fnAjaxUpdate();
-        }
 
         $scope.ChangeStatus = function(id) {
             var confirm = $mdDialog.confirm()
@@ -381,6 +318,7 @@
 
                 var params = {
                     id: id,
+                    UserName: $cookieStore.get('UserName')
                 }
                 $http.get($rootScope.RoutePath + "WalletTransaction/ApproveTransaction", {
                     params: params
@@ -392,7 +330,7 @@
                             .position('top right')
                             .hideDelay(3000)
                         );
-                        $scope.WalletTransaction(true);
+                        $scope.WalletTransactionReload(true);
                     } else {
                         $mdToast.show(
                             $mdToast.simple()
@@ -408,12 +346,13 @@
 
         $scope.RenewOrderService = function(id) {
             var confirm = $mdDialog.confirm()
-                .title('Are you sure you want to renew this order service?')
+                .title('Are you sure you want to renew this renew Transaction?')
                 .ok('Ok')
                 .cancel('Cancel')
             $mdDialog.show(confirm).then(function(ISConfirm) {
                 var params = {
                     id: id,
+                    UserName: $cookieStore.get('UserName')
                 }
                 $http.get($rootScope.RoutePath + "WalletTransaction/RenewTransaction", {
                     params: params
@@ -425,7 +364,7 @@
                             .position('top right')
                             .hideDelay(3000)
                         );
-                        $scope.WalletTransaction(true);
+                        $scope.WalletTransactionReload(true);
                         $mdDialog.hide();
                     } else {
                         $mdToast.show(
@@ -439,21 +378,6 @@
             });
         }
 
-        //Edit Wallet
-        // $scope.EditWalletById = function(id) {
-        //     $rootScope.FlgAddedEditlocal = true;
-        //     var o = _.findWhere($scope.lstWalletList, { id: id });
-        //     //$scope.GetAllStateByCountry(o.Countryid);
-        //     //$scope.GetAllCityByState(o.Stateid);          
-        //     $scope.model.id = o.id;
-        //     $scope.model.idUser = o.idUser;
-        //     $scope.model.Amount = o.Amount;
-        //     $scope.model.Remark = o.Remark;
-        //     $scope.model.Type = o.Type;
-        //     $scope.model.PaymentType = o.PaymentType;
-        //     $scope.model.OrderNumber = o.OrderNumber;
-        //     $scope.flag = true;
-        // }
 
         $scope.restForm = function() {
             $scope.formWalletTransaction.$setUntouched();
@@ -467,15 +391,6 @@
             $scope.FlgImage = false;
         }
 
-        // $scope.ResetTab = function() {
-        //     if ($rootScope.FlgAddedAccess != true) {
-        //         $rootScope.FlgAddedEditlocal = false;
-        //     }
-        // }
-        // $scope.GetSerch = function(Search) {
-        //     $scope.Search = Search;
-        //     $scope.GetAllUserWallet(true);
-        // }
         $scope.ResetModel = function() {
             $scope.model = {
                 id: 0,
@@ -486,87 +401,20 @@
                 OrderNumber: '',
                 IsPaymentSuccess: 0,
                 PaymentType: 'Offline',
+                Country: $cookieStore.get('UserCountry')
             };
 
             $scope.modelSearch = {
                 Search: ''
             }
-            $scope.restForm();
+
             $scope.flag = true;
             $scope.FlgImage = false;
+            $scope.GetAllAppInfo()
+            $scope.restForm();
+            $scope.apiMedia.removeAll();
         }
 
-        // $scope.ResetModel1 = function() {
-        //     $scope.model = {
-        //         id: 0,
-        //         idUser: '',
-        //         Amount: '',
-        //         Type: 'Credit',
-        //         Remark: '',
-        //         PaymentType: 'Admin',
-        //         OrderNumber: '',
-        //         CreatedDate: new Date(),
-        //         Createdby: $rootScope.UserName,
-        //     };
-        //     $scope.restForm();
-        //     $scope.flag = true;
-        // }
-
-        // //Reload Datatable
-
-        // $scope.Reset = function() {
-        //     $scope.model = {
-        //         id: 0,
-        //         idCountry: $scope.idCountry,
-        //         idState: '',
-        //         idCity: '',
-        //         LocationName: '',
-        //         Address: '',
-        //         Zipcode: '',
-        //         CreatedDate: new Date(),
-        //         Createdby: $rootScope.UserName,
-        //         Latitude: '',
-        //         Longitude: '',
-        //         Search: '',
-        //     };
-        //     $scope.GetAllStateByCountry($scope.model.idCountry)
-        //     $scope.restForm();
-
-        // }
-
-        // $scope.ResetSearch = function() {
-        //     $scope.tab = { selectedIndex: 0 };
-        //     $scope.modelSearch = {
-        //         idCountry: 'All',
-        //         idState: 'All',
-        //         idCity: 'All',
-        //         IsActive: 'All',
-        //         idRegion: 'All',
-
-        //     }
-        //     $scope.lstSearchState = '';
-        //     $scope.lstSearchCity = '';
-        //     $scope.GetAllUserWallet(true);
-        // }
-        // $scope.gotoWalletList = function() {
-        //     $scope.model = {
-        //         id: 0,
-        //         idUser: '',
-        //         Amount: '',
-        //         Type: 'Credit',
-        //         Remark: '',
-        //         PaymentType: 'Admin',
-        //         OrderNumber: '',
-        //         CreatedDate: new Date(),
-        //         Createdby: $rootScope.UserName,
-        //     };
-
-        //     $scope.modelSearch = {
-        //         Search: ''
-        //     }
-        //     $scope.flag = false
-        //     $scope.GetAllUserWallet(true);
-        // }
         $scope.init();
     }
 })();
