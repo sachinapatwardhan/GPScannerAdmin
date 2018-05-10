a232fb47c2ac6d8cbe7521b5d271d3cf27d24d26
dhaval <dhaval@bugzstudio.com>
2018-05-10 10:15:33 +0000

    git-svn-id: https://svn.bugzstudio.com:7777/svn/GPScannerAdmin@513 3ff773e8-363c-db4a-86ae-61a296765148

---
+++ dist/server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+// viewed at http://localhost:8080
+
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+
+app.use(express.static(__dirname + '/'));
+app.listen(7172);
+console.log("server running on 7172");
