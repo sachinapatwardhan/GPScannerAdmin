1c4440fee9bac0fd4a7c4cb1ea3b262d514fb396
soham <soham.patel@bugzstudio.com>
2020-07-13 16:59:53 +0530

    Build Admin with change version number

--- src/app/index.run.js
+++ src/app/index.run.js
@@ -19,7 +19,7 @@
         //} catch (ex) {
 
         //}
-        $rootScope.VersionNumber = "20.02.0608.128db22c";
+        $rootScope.VersionNumber = "20.03.0713.80cebcde";
         // $rootScope.RoutePath = "http://localhost:7212/";
         // $rootScope.Socket_URL = "http://localhost:7212";
 
